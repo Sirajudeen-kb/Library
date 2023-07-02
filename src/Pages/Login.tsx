2da788b19d7f84629b2de40321b9012385fe8a2a
Sirajudeen K.B <kbsiraj12@gmail.com>
2023-07-02 09:02:16 +0530

    Bg image login and signup

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -7,7 +7,7 @@ function Login() {
     <>
       {" "}
       <Navbar />
-<div className="container-l background-image">
+<div className="container-l background-image" style={{ backgroundImage: 'url("./images/Signup.jpg")' }} >
     <div className="box">  
       <form className="form-container">
         <div className="mb-3">
